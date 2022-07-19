df3f1d0e56e2cae77c78205bae438b3f226e270c
Taos Transue <spacechips@rocketmail.com>
2022-07-18 18:15:18 -0700

    bash to python

--- .github/workflows/write_to_database.py
+++ .github/workflows/write_to_database.py
@@ -1,14 +1,39 @@
 import sys
+import requests
 import base64
 import json
 
 
 BASE_DIR = '../..'
+DATABASE_URL = 'https://api.github.com/repos/reepoi/ivcurves/contents/database.json'
 
 
 def load_json(filename):
     with open(f'{BASE_DIR}/{filename}.json', 'r') as file:
         return json.load(file)
+
+
+def get_database_sha(GITHUB_TOKEN):
+    res = requests.get(DATABASE_URL, headers={'Accept': 'application/vnd.github+json',
+                               'Authorization': f'token {GITHUB_TOKEN}'})
+    return res.json()['sha']
+
+
+def commit_scores_to_database(database):
+    database['test'] = 'hi2'
+
+
+def push_new_database(database_b64, GITHUB_TOKEN):
+    database_sha = get_database_sha(GITHUB_TOKEN)
+    res = requests.put(DATABASE_URL,
+                       headers={'Accept': 'application/vnd.github+json',
+                                'Authorization': f'token {GITHUB_TOKEN}'},
+                       data={'message':'update database',
+                             'committer': {'name':'reepoi',
+                                           'email':'reepoi@ivcurves'},
+                             'content': f'{database_b64}',
+                             'sha':f'{database_sha}'})
+    print(res.json())
 
 
 assert len(sys.argv) == 2, 'Please pass a GITHUB_TOKEN'
@@ -18,18 +43,9 @@ GITHUB_TOKEN = sys.argv[1]
 database = load_json('database')
 scores = load_json('scores.json/scores')
 
-database['test'] = 'hi'
+commit_scores_to_database(database)
 
-database_base64 = str(base64.b64encode(json.dumps(database).encode('ascii')))[2:-1]
+database_b64 = str(base64.b64encode(json.dumps(database).encode('ascii')))[2:-1]
 
-bash_cmd_update_database = (
-'curl '
-'-X PUT '
-'-H "Accept: application/vnd.github+json" '
-f'-H "Authorization: token {GITHUB_TOKEN}" '
-'https://api.github.com/repos/reepoi/ivcurves/contents/database.json '
-).replace('"', '\'')
-bash_cmd_update_database += \
-    f'-d \'{{"message":"update database","committer":{{"name":"reepoi","email":"reepoi@ivcurves"}},"content":"{database_base64}","sha":"63b1f9d549b40900dc4d107f832ce251b0dcbab2"}}\''
-print(bash_cmd_update_database)
+push_new_database(database_b64, GITHUB_TOKEN)
 
