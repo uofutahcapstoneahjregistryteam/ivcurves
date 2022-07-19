288a013fbe33c94c9994c8e805bd8eb3ff19d3be
Taos Transue <spacechips@rocketmail.com>
2022-07-19 09:44:23 -0700

    adding argparse

--- .github/workflows/write_to_database.py
+++ .github/workflows/write_to_database.py
@@ -2,6 +2,7 @@ import sys
 import requests
 import base64
 import json
+import argparse
 
 
 def load_json(filename):
@@ -27,21 +28,29 @@ def push_new_database(database_b64, GITHUB_HEADERS):
                                            'email':'reepoi@ivcurves'},
                              'content': f'{database_b64}',
                              'sha': f'{database_sha}'})
-    print(res.json())
+    res.raise_for_status()
 
 
-BASE_DIR = '../..'
-DATABASE_URL = 'https://api.github.com/repos/reepoi/ivcurves/contents/database.json'
-assert len(sys.argv) == 2, 'Please pass a GITHUB_TOKEN'
-GITHUB_TOKEN = sys.argv[1]
-GITHUB_HEADERS = {'Accept': 'application/vnd.github+json',
-                  'Authorization': f'token {GITHUB_TOKEN}'}
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--github-token', dest='github_token', type=str,
+                        help='secrets.GITHUB_TOKEN (or github.token) of the workflow run')
+    parser.add_argument('--actor', dest='committer', type=str,
+                        help='github.actor of the workflow run')
+    cmd_args = parser.parse_args()
 
-database = load_json(f'{BASE_DIR}/database')
-scores_json = load_json('scores.json/scores')
 
-write_scores_to_database(database, scores_json)
+    BASE_DIR = '../..'
+    DATABASE_URL = 'https://api.github.com/repos/reepoi/ivcurves/contents/database.json'
+    GITHUB_TOKEN = cmd_args.github_token
+    COMMITTER = cmd_args.committer
+    GITHUB_HEADERS = {'Accept': 'application/vnd.github+json',
+                      'Authorization': f'token {GITHUB_TOKEN}'}
 
-database_b64 = str(base64.b64encode(json.dumps(database).encode('ascii')))[2:-1]
-push_new_database(database_b64, GITHUB_HEADERS)
+    database = load_json(f'{BASE_DIR}/database')
+    scores_json = load_json('scores.json/scores')
+    write_scores_to_database(database, scores_json)
+
+    database_b64 = str(base64.b64encode(json.dumps(database).encode('ascii')))[2:-1]
+    push_new_database(database_b64, GITHUB_HEADERS)
 
