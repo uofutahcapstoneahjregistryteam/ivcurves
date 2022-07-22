a8632293ee0b8604acf818a546455c3abcf7385e
uofutahcapstoneahjregistryteam <77124507+uofutahcapstoneahjregistryteam@users.noreply.github.com>
2022-07-22 11:56:37 -0700

    Update solution.py

--- uofutahcapstoneahjregistryteam/solution.py
+++ uofutahcapstoneahjregistryteam/solution.py
@@ -7,7 +7,7 @@ parameters = 1 + np.zeros((32, 6))
 header = 'Index,photocurrent,saturation_current,resistance_series,resistance_shunt,n,cells_in_series'.split(',')
 
 for name in ['case1.csv', 'case2.csv']:
-    with open(f'./reepoi/{name}', 'w') as file:
+    with open(f'./uofutahcapstoneahjregistryteam/{name}', 'w') as file:
         writer = csv.writer(file, delimiter=',')
         writer.writerow(header)
         for idx, row in enumerate(parameters):
